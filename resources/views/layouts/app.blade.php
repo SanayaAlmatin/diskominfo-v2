4766d0dddef23dd16025998c33516ee3fa4fbc1e
axxpxmd <asiphamdi@gmail.com>
2026-05-04 17:53:44 +0700

    refactor: update meta tags and site variables in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,44 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
+    @php
+        $siteTitle = trim($__env->yieldContent('title', 'Diskominfo Tangerang Selatan'));
+        $siteDescription = trim($__env->yieldContent('meta_description', 'Portal resmi Dinas Komunikasi dan Informatika Kota Tangerang Selatan untuk informasi publik, layanan digital, data, dan infrastruktur teknologi informasi.'));
+        $siteKeywords = trim($__env->yieldContent('meta_keywords', 'Diskominfo Tangerang Selatan, Kominfo Tangsel, Pemerintah Kota Tangerang Selatan, layanan digital, informasi publik, smart city, satu data'));
+        $siteRobots = trim($__env->yieldContent('meta_robots', 'index, follow'));
+        $siteUrl = url()->current();
+        $siteImage = asset('Images/logo-kominfo.png');
+    @endphp
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Diskominfo Tangerang Selatan')</title>
+    <meta name="description" content="{{ $siteDescription }}">
+    <meta name="keywords" content="{{ $siteKeywords }}">
+    <meta name="author" content="Dinas Komunikasi dan Informatika Kota Tangerang Selatan">
+    <meta name="robots" content="{{ $siteRobots }}">
+    <meta name="theme-color" content="#044FA0">
+    <meta name="application-name" content="Portal Diskominfo Tangerang Selatan">
+
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:site_name" content="Portal Diskominfo Tangerang Selatan">
+    <meta property="og:title" content="{{ $siteTitle }}">
+    <meta property="og:description" content="{{ $siteDescription }}">
+    <meta property="og:url" content="{{ $siteUrl }}">
+    <meta property="og:image" content="{{ $siteImage }}">
+    <meta property="og:image:alt" content="Logo Diskominfo Kota Tangerang Selatan">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="{{ $siteTitle }}">
+    <meta name="twitter:description" content="{{ $siteDescription }}">
+    <meta name="twitter:image" content="{{ $siteImage }}">
+
+    <link rel="canonical" href="{{ $siteUrl }}">
+    <link rel="icon" type="image/png" href="{{ $siteImage }}">
+    <link rel="shortcut icon" type="image/png" href="{{ $siteImage }}">
+    <link rel="apple-touch-icon" href="{{ $siteImage }}">
+
+    <title>{{ $siteTitle }}</title>
 
     <!-- Google Fonts: Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
