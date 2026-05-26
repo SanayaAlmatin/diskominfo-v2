df85f7d2495a0303c4b1564587a51fc80c321942
SanayaAlmatin <sanayaalmatin@gmail.com>
2026-05-26 15:39:37 +0700

    chore: update gitignore dan hapus file test dari repositori remote

--- test_images.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$images = \App\Models\TrNewsImage::take(5)->get();
-foreach ($images as $img) {
-    echo $img->image . "\n";
-}
