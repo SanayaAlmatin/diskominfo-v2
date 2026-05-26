92c1909045d3e8e56c3549279f752117cc2fdc71
SanayaAlmatin <sanayaalmatin@gmail.com>
2026-05-26 15:54:33 +0700

    feat: add admin dashboard with user welcome banner, statistical cards, and quick navigation links

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -135,9 +135,9 @@
                 @php
                     $links = [
                         [
-                            'label' => 'Banner',
+                            'label' => 'Galeri Foto',
                             'sub'   => 'Konten Portal',
-                            'route' => 'admin.program-vacancy.create',
+                            'route' => 'admin.foto.index',
                             'color' => '#4F46E5',
                             'icon'  => 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
                         ],
